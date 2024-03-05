0d8e1ccb7b258510a7d0b0209ca4d912190a1d7a
Muhammad Mutahhar <muhammad.mutahhar@koderlabs.com>
2024-03-05 17:41:45 +0500

    minor fixes

---
+++ src/Migrators/Migrators.MSSQL/Migrations/Application/20240216124504_RemoveMultitenancy.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrators.MSSQL.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class RemoveMultitenancy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "Catalog");
+
+            migrationBuilder.DropTable(
+                name: "Brands",
+                schema: "Catalog");
+
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                schema: "Identity",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserLogins",
+                schema: "Identity",
+                table: "UserLogins");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserLogins_LoginProvider_ProviderKey_TenantId",
+                schema: "Identity",
+                table: "UserLogins");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                schema: "Identity",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserTokens");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "Identity",
+                table: "UserLogins");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserLogins");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserClaims");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Identity",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "Identity",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "Group",
+                schema: "Identity",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                schema: "Identity",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedOn",
+                schema: "Identity",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Identity",
+                table: "RoleClaims");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                schema: "Auditing",
+                table: "AuditTrails");
+
+            migrationBuilder.EnsureSchema(
+                name: "auditing");
+
+            migrationBuilder.EnsureSchema(
+                name: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserTokens",
+                schema: "Identity",
+                newName: "UserTokens",
+                newSchema: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                schema: "Identity",
+                newName: "Users",
+                newSchema: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                schema: "Identity",
+                newName: "UserRoles",
+                newSchema: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserLogins",
+                schema: "Identity",
+                newName: "UserLogins",
+                newSchema: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserClaims",
+                schema: "Identity",
+                newName: "UserClaims",
+                newSchema: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                schema: "Identity",
+                newName: "Roles",
+                newSchema: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "RoleClaims",
+                schema: "Identity",
+                newName: "RoleClaims",
+                newSchema: "identity");
+
+            migrationBuilder.RenameTable(
+                name: "AuditTrails",
+                schema: "Auditing",
+                newName: "AuditTrails",
+                newSchema: "auditing");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserLogins",
+                schema: "identity",
+                table: "UserLogins",
+                columns: new[] { "LoginProvider", "ProviderKey" });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "identity",
+                table: "Users",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                schema: "identity",
+                table: "Roles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                schema: "identity",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserLogins",
+                schema: "identity",
+                table: "UserLogins");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                schema: "identity",
+                table: "Roles");
+
+            migrationBuilder.EnsureSchema(
+                name: "Auditing");
+
+            migrationBuilder.EnsureSchema(
+                name: "Catalog");
+
+            migrationBuilder.EnsureSchema(
+                name: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserTokens",
+                schema: "identity",
+                newName: "UserTokens",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                schema: "identity",
+                newName: "Users",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                schema: "identity",
+                newName: "UserRoles",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserLogins",
+                schema: "identity",
+                newName: "UserLogins",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "UserClaims",
+                schema: "identity",
+                newName: "UserClaims",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                schema: "identity",
+                newName: "Roles",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "RoleClaims",
+                schema: "identity",
+                newName: "RoleClaims",
+                newSchema: "Identity");
+
+            migrationBuilder.RenameTable(
+                name: "AuditTrails",
+                schema: "auditing",
+                newName: "AuditTrails",
+                newSchema: "Auditing");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserTokens",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Identity",
+                table: "Users",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserRoles",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                schema: "Identity",
+                table: "UserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserLogins",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Identity",
+                table: "UserClaims",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Identity",
+                table: "Roles",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "Identity",
+                table: "RoleClaims",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Group",
+                schema: "Identity",
+                table: "RoleClaims",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                schema: "Identity",
+                table: "RoleClaims",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedOn",
+                schema: "Identity",
+                table: "RoleClaims",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Identity",
+                table: "RoleClaims",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TenantId",
+                schema: "Auditing",
+                table: "AuditTrails",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserLogins",
+                schema: "Identity",
+                table: "UserLogins",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                schema: "Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    TenantId = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "Catalog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BrandId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImagePath = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: true),
+                    LastModifiedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false),
+                    Rate = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TenantId = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalSchema: "Catalog",
+                        principalTable: "Brands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "Identity",
+                table: "Users",
+                columns: new[] { "NormalizedUserName", "TenantId" },
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLogins_LoginProvider_ProviderKey_TenantId",
+                schema: "Identity",
+                table: "UserLogins",
+                columns: new[] { "LoginProvider", "ProviderKey", "TenantId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                schema: "Identity",
+                table: "Roles",
+                columns: new[] { "NormalizedName", "TenantId" },
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandId",
+                schema: "Catalog",
+                table: "Products",
+                column: "BrandId");
+        }
+    }
+}
